1a9162e1bae83b228d989d0a4f219ecc671ed8f6
Hardik Sarkar <107950304+hardiksarkar@users.noreply.github.com>
2023-08-25 08:57:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let boldText = Array.from(document.getElementsByTagName("strong"));
 function highlight() {
     //Write your code here
-
+	boldText.forEach((elem)=>{
+        elem.style.color="green";
+    })
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+boldText.forEach((elem)=>{
+        elem.style.color="black";
+    })
     
 }
